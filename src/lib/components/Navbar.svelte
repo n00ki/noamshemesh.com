caf486daf62745e4e5e16b84855b84ed8721757b
noam <shemeshno@gmail.com>
2021-08-11 02:32:18 +0300

    Add global stylesheet

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,7 +1,9 @@
 <header>
 	<nav class="flex justify-betweenc items-end">
-		<h1 class="flex-1 font-black text-5xl">noam.</h1>
-		<a href="/about" class="px-2 text-2xl">About</a>
-		<a href="/blog" class="px-2 text-2xl">Blog</a>
+		<a href="/">
+			<h1 class="flex-1 font-black text-5xl">noam.</h1>
+		</a>
+		<a href="/about" class="btn-nav">About</a>
+		<a href="/blog" class="btn-nav">Blog</a>
 	</nav>
 </header>
