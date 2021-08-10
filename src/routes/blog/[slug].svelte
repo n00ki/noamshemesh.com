6b429c431d2dca5aec4dafa5c4b87091aaf8de50
noam <shemeshno@gmail.com>
2021-08-10 21:13:53 +0300

    progress

---
+++ src/routes/blog/[slug].svelte
@@ -0,0 +1,17 @@
+<script context="module">
+	export async function load({ page }) {
+		let { slug } = page.params;
+
+		return {
+			props: {
+				slug
+			}
+		};
+	}
+</script>
+
+<script>
+	export let slug;
+</script>
+
+<h1>{slug}</h1>
