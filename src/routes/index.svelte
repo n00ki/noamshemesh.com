178beca2c8a7542217f7325edc6e6697571d4980
noam <shemeshno@gmail.com>
2021-08-12 10:02:40 +0300

    Improve homepage layout

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,14 +1,21 @@
-<section class="grid grid-cols-2">
-	<div class="text-center">
-		<h1>Hi! I am Noam.</h1>
-		<p>
-			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat, repudiandae! Cupiditate
-			provident repellendus doloribus numquam voluptates illo. Sint atque doloremque sed at,
-			officiis iure sit harum odio nihil itaque soluta!
+<div class="flex">
+	<div class="flex-1 flex flex-col text-center justify-center">
+		<h1 class="">Hi, I'm Noam! &#x1F44B</h1>
+		<p class="">
+			Music Producer, Sound Engineer & Tech Enthusiast based in <strong>Tel Aviv</strong>. Checkout
+			my recent, pick my brain in the
+			<a href="/blog">
+				<span class="inline-flex items-center px-3 py-0.5 rounded-lg bg-pink-100 text-pink-800">
+					Blog
+				</span>
+			</a>, or simply say <strong>Hi!</strong>
 		</p>
 	</div>
-	<img
-		src="https://res.cloudinary.com/nshemesh/image/upload/v1628635520/noamshemesh.com/noam.png"
-		alt="noam"
-	/>
-</section>
+
+	<div class="flex-1">
+		<img
+			src="https://res.cloudinary.com/nshemesh/image/upload/v1628635520/noamshemesh.com/noam.png"
+			alt="noam"
+		/>
+	</div>
+</div>
