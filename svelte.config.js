d385d828f6c8634cfdf1d0bfa026b706ff5cc4d3
noam <shemeshno@gmail.com>
2021-08-14 19:37:48 +0300

    add static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,3 +1,4 @@
+import adapter from '@sveltejs/adapter-static';
 import { mdsvex } from 'mdsvex';
 import mdsvexConfig from './mdsvex.config.js';
 import preprocess from 'svelte-preprocess';
@@ -6,6 +7,12 @@ const config = {
 	extensions: ['.svelte', ...mdsvexConfig.extensions],
 
 	kit: {
+		adapter: adapter({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		}),
 		target: '#svelte'
 	},
 
