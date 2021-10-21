83bdba36685af5fea8f683b3aad8f3e865f16b94
noam <shemeshno@gmail.com>
2021-10-21 14:09:57 +0300

    remove unnecessary files for current prod

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -1,4 +1,4 @@
-<h1>Sorry! Something went wrong :(</h1>
+<h1>Oops! Something went wrong :(</h1>
 <a href="/">
 	<p>Go back Home</p>
 </a>
--- src/routes/projects/index.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-	// Components
-	import SEO from '$lib/components/SEO.svelte';
-</script>
-
-<SEO pageTitle="Projects" />
-
-<h1>Projects</h1>
