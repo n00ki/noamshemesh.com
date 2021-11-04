d6713fcc86ee408fa9e2c563825420ea49c51ff0
noam <shemeshno@gmail.com>
2021-11-04 14:04:24 +0200

    improve homepage

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -8,26 +8,34 @@
 <div class="flex flex-col-reverse px-2 lg:flex-row lg:justify-evenly 2xl:px-24">
 	<section class="flex-1 flex flex-col text-center justify-center">
 		<h1 class="text-4xl pb-3 font-extrabold sm:text-5xl sm:py-4">Hi, I'm Noam! &#x1F44B</h1>
-		<p class="text-lg leading-6 px-1 sm:text-xl sm:leading-7 sm:px-0">
-			Music Producer, Web Developer & Life Enthusiast based in <strong>Tel Aviv</strong>. Welcome to
-			my little corner of the internet.
+		<p class="text-lg leading-6 px-1 md:text-xl sm:leading-7 sm:px-0">
+			Music Producer, Web Developer & Life Enthusiast based in <strong>Tel Aviv</strong>. <br />
+			Welcome to my little corner of the internet. <br />
+			Checkout my recent
+			<a href="/projects">
+				<span
+					class="text-transparent bg-clip-text bg-gradient-to-r from-pink-500 via-pink-700 to-indigo-500 hover:bg-gradient-to-r hover:from-indigo-600 hover:via-pink-700 hover:to-pink-500"
+					>Projects</span
+				></a
+			>
+			, or simply say Hi :)
 		</p>
 
 		<span class="w-full py-8 inline-flex space-x-4 text-2xl justify-center">
 			<a href="mailto:noams@hey.com">
-				<i class="bi bi-envelope-fill" />
+				<i class="bi bi-envelope-fill transform ease-in-out duration-500 hover:text-pink-600" />
 			</a>
 			<a href="https://twitter.com/shemeshno" target="_blank">
-				<i class="bi bi-twitter" />
+				<i class="bi bi-twitter transform ease-in-out duration-500 hover:text-pink-600" />
 			</a>
 			<a href="https://www.facebook.com/noam.shemesh.526" target="_blank">
-				<i class="bi bi-facebook" />
+				<i class="bi bi-facebook transform ease-in-out duration-500 hover:text-pink-600" />
 			</a>
 			<a href="https://www.instagram.com/noamshemesh/" target="_blank">
-				<i class="bi bi-instagram" />
+				<i class="bi bi-instagram transform ease-in-out duration-500 hover:text-pink-600" />
 			</a>
 			<a href="https://github.com/n00ki" target="_blank">
-				<i class="bi bi-github" />
+				<i class="bi bi-github transform ease-in-out duration-500 hover:text-pink-600" />
 			</a>
 		</span>
 	</section>
