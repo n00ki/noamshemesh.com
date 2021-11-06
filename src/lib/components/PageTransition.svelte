a04fc0293d0a4fe3bc14a8ae0f8d9ae96400dbe3
noam <shemeshno@gmail.com>
2021-11-06 19:25:44 +0200

    add page tranisions

---
+++ src/lib/components/PageTransition.svelte
@@ -0,0 +1,13 @@
+<script>
+	// Transitions
+	import { fade } from 'svelte/transition';
+
+	// Props
+	export let refresh = '';
+</script>
+
+{#key refresh}
+	<div in:fade={{ duration: 250, delay: 250 }}>
+		<slot />
+	</div>
+{/key}
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,9 +2,13 @@
 	// Styles
 	import '../app.postcss';
 
+	// Stores
+	import { page } from '$app/stores';
+
 	// Components
 	import SEO from '$lib/components/SEO.svelte';
 	import Navbar from '$lib/components/Navbar.svelte';
+	import PageTransition from '$lib/components/PageTransition.svelte';
 </script>
 
 <SEO />
@@ -12,5 +16,7 @@
 <main class="container px-2 mx-auto text-gray-50 font-poppins">
 	<Navbar />
 
-	<slot />
+	<PageTransition refresh={$page.path}>
+		<slot />
+	</PageTransition>
 </main>
