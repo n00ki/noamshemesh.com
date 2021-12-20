42d1b8460a612a1186ef5c52f3398dbb1cde63ea
noam <shemeshno@gmail.com>
2021-12-20 12:05:50 +0200

    improve error layout

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -1,4 +1,6 @@
-<h1>Oops! Something went wrong :(</h1>
-<a href="/">
-	<p>Go back Home</p>
-</a>
+<div class="text-center">
+	<h1>Oops! Something went wrong :(</h1>
+	<a href="/">
+		<p>Go back Home</p>
+	</a>
+</div>
